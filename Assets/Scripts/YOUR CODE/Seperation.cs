72c02a0b5dabce4fa3239121de4f03adb8661d8a
ProjectHexx15 <amieam.mcgrory@gmail.com>
2025-11-09 16:06:25 +0000

    Sepperation Script
    Seperation Script Added

---
+++ Assets/Scripts/YOUR CODE/Seperation.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Seperation : SteeringBehaviour
+{
+    public override Vector3 UpdateBehaviour(SteeringAgent steeringAgent)
+    {
+
+
+
+        return desiredVelocity;
+    }
+
+}
