c870022ed81f185c7072012be2e47255b47aeb64
ProjectHexx15 <amieam.mcgrory@gmail.com>
2025-12-03 13:49:32 +0000

    References

--- Assets/Scripts/YOUR CODE/References.cs
+++ Assets/Scripts/YOUR CODE/References.cs
@@ -1,10 +1,10 @@
 /*
 List references here in Harvard format:
 
+Buckland, M. (2004) Programming Game AI by Example. Jones & Bartlett Learning.
 
+DeepWiki (2025) Target-following Behaviours. Available at: https://deepwiki.com/pushbuttonreceivecode/Steeriously/3.2-target-following-behaviors (Accessed 3 November 2025).
 
-
-
-
+Zeus GameDev Academy (2024) AI Groups And Formations in Unity C# - Game AI Mastery Part 2. Available at: https://www.youtube.com/watch?v=1VWpFk7wEEM (Accessed 9 Novemeber 2025).
 
 */
